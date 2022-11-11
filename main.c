92f468ba6d73ad91c4b56c835b69b654da20372b
adham612 <adhammohamedelsayed6@gmail.com>
2022-11-11 20:29:48 +0200

    v1

---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+
+ch arr[20]={"Ahmed"};
+printf("%s",arr[20]);
---
+++ math.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+
+int x=10;
+int y=5;
+int temp =x+y;
+printf("%d",temp);
